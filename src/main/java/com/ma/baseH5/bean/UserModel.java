be4fa02eec08ca71d5ce8acf78ea404d63fb85ac
Jamie <pengshuai.ma@magustek.com>
2019-04-23 14:32:18 +0800

    fetchCommit1

--- src/main/java/com/ma/baseH5/bean/UserModel.java
+++ src/main/java/com/ma/baseH5/bean/UserModel.java
@@ -7,4 +7,28 @@ public class UserModel {
     private String username;
     private String password;
     private String org;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getOrg() {
+        return org;
+    }
+
+    public void setOrg(String org) {
+        this.org = org;
+    }
 }
