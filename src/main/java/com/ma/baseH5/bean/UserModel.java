2265091545e3d44be415d9985f0503d5b9195958
Jamie <pengshuai.ma@magustek.com>
2019-04-23 15:09:53 +0800

    fetchLocal_1

--- src/main/java/com/ma/baseH5/bean/UserModel.java
+++ src/main/java/com/ma/baseH5/bean/UserModel.java
@@ -7,6 +7,7 @@ public class UserModel {
     private String username;
     private String password;
     private String org;
+    private String favorite;
 
     public String getUsername() {
         return username;
